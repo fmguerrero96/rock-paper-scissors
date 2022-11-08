1d1271c7864d73a0fa8d4878e8795a3a2c1779cb
Misael Guerrero <mguerrerowest@gmail.com>
2022-11-08 17:02:27 -0500

    Create game loop to play 5 rounds and keep score

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const choices = ['rock', 'paper', 'scissors'];
+const tieMessage = ("It's a tie.")
+const winMessage = ('You WIn!!!')
+const loseMessage = ('You Lose.')
+
+let rounds = 0
+let playerWin = 0;
+let compWin = 0;
+let tie = 0;
 
 function getCompChoice() {
     let selection = choices[Math.floor(Math.random()*choices.length)];
@@ -12,22 +20,33 @@ function playerChoice() {
 }
 
 function rpsRound(humanHand, compHand) {
-    const tieMessage = ("It's a tie.")
-    const winMessage = ('You WIn!!!')
-    const loseMessage = ('You Lose.')
     console.log('Human Player: ', humanHand);
     console.log('Computer Player: ', compHand);
     if (humanHand === compHand) {
+        rounds += 1
+        tie += 1
         return (tieMessage);
     } else if ((humanHand==='rock' && compHand==='scissors') || 
                 (humanHand==='paper' && compHand==='rock') || 
                 (humanHand==='scissors' && compHand==='paper')) {
+                    rounds += 1
+                    playerWin += 1
                     return (winMessage);
                 }
-        else {return (loseMessage)}
+        else {
+            rounds += 1
+            compWin += 1
+            return (loseMessage)}
+    
 }
 
+function game(){
+    while (rounds < 5){
+        rpsRound(playerChoice(), getCompChoice());
+    }
+    console.log('Player Score:', playerWin)
+    console.log('Computer Score:', compWin)
+    console.log('Ties:', tie)
+}
 
-
-
-
+game()
